89e6ea4bd3d193971008a436effd2de24def789f
Jonathan <jonathanestebangalan@gmail.com>
2020-12-13 17:25:18 +0100

    fin de estilos desktop

--- src/App.js
+++ src/App.js
@@ -3,35 +3,35 @@ import './App.css';
 function Header() {
   return (
     <header className="header">
-      <h1 className="title">Join our community</h1>
-      <p className="text">30-day, hassle-free money backguarantee</p>
-      <p className="text">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+      <h1 className="header__title">Join our community</h1>
+      <p className="header__text">30-day, hassle-free money backguarantee</p>
+      <p className="header__text2">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
     </header>
   )
 }
 
 function Info() {
   return (
-    <section className="section">
-      <h2 className="subtitle">Monthly Subscription</h2>
-      <p className="section__text">$29 <span className="span">per month</span></p>
-      <p className="section__info">Full access for less than $1 a day</p>
-      <button className="button">Sign Up</button>
+    <section className="section-info">
+      <h2 className="info__subtitle">Monthly Subscription</h2>
+      <p className="info__price">$29 <span className="info__span">per month</span></p>
+      <p className="info__text">Full access for less than $1 a day</p>
+      <button className="info__button">Sign Up</button>
     </section>
   )
 }
 
 function About() {
   return (
-    <section className="about">
-      <h2>Why Us</h2>
-      <p>Tutorials by industry experts</p>
-      <p>Peer and experts code review</p>
-      <p>Coding exercises</p>
-      <p>Access to our Github repos</p>
-      <p>Community forum</p>
-      <p>Flashcard decks</p>
-      <p>New videos every weeks</p>
+    <section className="section-about">
+      <h2 className="about__title">Why Us</h2>
+      <p className="about__text">Tutorials by industry experts</p>
+      <p className="about__text">Peer and experts code review</p>
+      <p className="about__text">Coding exercises</p>
+      <p className="about__text">Access to our Github repos</p>
+      <p className="about__text">Community forum</p>
+      <p className="about__text">Flashcard decks</p>
+      <p className="about__text">New videos every weeks</p>
     </section>
   )
 }
